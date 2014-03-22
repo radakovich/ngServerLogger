8db690c103d540afce9bf5454a1d62d38e97b775
jake <jradakov@gmail.com>
2014-03-21 23:50:07 -0500

    added the enum for logging levels as well as the unit test

--- test/configSpec.js
+++ test/configSpec.js
@@ -1,7 +1,10 @@
 describe('ngLoggerServer', function(){
     beforeEach(module('radakovich.ngServerLogger'));
 
-    it('2 + 2 should equal 4', function(){
-        expect(2+2).toEqual(4);
-    });
+    it('should have a LOGGING_LEVELS enum', inject(function(LOGGING_LEVELS){
+        expect(LOGGING_LEVELS.ERROR).toEqual({name: 'ERROR', level: 4});
+        expect(LOGGING_LEVELS.WARN).toEqual({name: 'WARN', level: 3});
+        expect(LOGGING_LEVELS.INFO).toEqual({name: 'INFO', level: 2});
+        expect(LOGGING_LEVELS.DEBUG).toEqual({name: 'DEBUG', level: 1});
+    }));
 });
